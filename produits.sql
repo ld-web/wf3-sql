5667e24455fd41f7a3f9d61bf3b1cc0b211e24b8
Lucas Delobelle <lucas@ld-web.net>
2021-10-27 16:44:42 +0200

    INSERT produits

---
+++ produits.sql
@@ -0,0 +1,154 @@
+INSERT INTO `produit` (
+    `id`,
+    `nom`,
+    `prix_ht`,
+    `content_desc`,
+    `illustration`,
+    `quantite`,
+    `rayon_id`
+  )
+VALUES (
+    1,
+    'Pantalon',
+    '999.99',
+    "L'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble.",
+    'pantalon.png',
+    50,
+    NULL
+  ),
+  (
+    2,
+    'Kiwi',
+    '75.33',
+    'la bureautique informatique, sans que son contenu ne soit modifié.',
+    'kiwi.png',
+    100,
+    25
+  ),
+  (
+    3,
+    'Pain',
+    '999.99',
+    'Depuis les années 1500, quand un imprimeur anonyme assembla ensemble.',
+    'pain.png',
+    250,
+    10
+  ),
+  (
+    4,
+    'Tacos',
+    '500.45',
+    'Quand un imprimeur anonyme assembla ensemble.',
+    'tacos.png',
+    800,
+    21
+  ),
+  (
+    5,
+    'Chemise',
+    '100.00',
+    'Limprimerie depuis les années 1500.',
+    'chemise.png',
+    10,
+    26
+  ),
+  (
+    6,
+    'Costume',
+    '275.00',
+    NULL,
+    'costume.png',
+    78,
+    26
+  ),
+  (
+    7,
+    'Lunette',
+    '999.99',
+    'Lextrait standard de Lorem Ipsum utilisé depuis le XVIè siècle.',
+    'lunette.png',
+    70,
+    NULL
+  ),
+  (
+    8,
+    'Ceinture',
+    '500.00',
+    'Lorem Ipsum, vous devez être sûr quil ny a rien dembarrassant caché dans le texte.',
+    'ceinture.png',
+    50,
+    26
+  ),
+  (
+    9,
+    'Chaussure',
+    '999.99',
+    'un imprimeur anonyme assembla ensemble maman de.',
+    'chaussure.png',
+    40,
+    26
+  ),
+  (
+    10,
+    'poudre',
+    '999.99',
+    'anonyme assembla les erreurs du passé.',
+    'poudre.png',
+    80,
+    9
+  ),
+  (11, 'Sucre', '450.00', NULL, NULL, 56, NULL),
+  (
+    12,
+    'Sel',
+    '540.00',
+    'Plusieurs versions sont apparues avec le temps.',
+    'sel.png',
+    33,
+    NULL
+  ),
+  (
+    13,
+    'Savon',
+    '200.00',
+    'Depuis quelques années,un imprimeur cherche le sous',
+    'savon.png',
+    89,
+    NULL
+  ),
+  (
+    14,
+    'Biscuit',
+    '750.00',
+    NULL,
+    'biscuit.png',
+    45,
+    NULL
+  ),
+  (
+    15,
+    'Gesier',
+    '900.00',
+    'un imprimeur anonyme va vers les clients',
+    NULL,
+    90,
+    4
+  ),
+  (
+    16,
+    'Poulet',
+    '600.00',
+    'Limprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble.',
+    'poulet.png',
+    20,
+    4
+  ),
+  (
+    17,
+    'Manque',
+    '300.00',
+    'Contrairement à une opinion répandue, le Lorem Ipsum',
+    NULL,
+    1000,
+    NULL
+  );
